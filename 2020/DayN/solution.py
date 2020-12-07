34cff98cfbcbbc95e01bfaf65f59971828d91402
irongiant33 <samuelmiller0714@gmail.com>
2020-12-07 13:40:09 -0500

    simplified the daily skeleton code

--- 2020/DayN/solution.py
+++ 2020/DayN/solution.py
@@ -1,11 +1,5 @@
 INPUT_FILE_NAME = "input.txt"
 
-def get_input_list():
-	input_file = open(INPUT_FILE_NAME)
-	input_file_list = []
-	for line in input_file:
-		input_file_list.append(line.rstrip())
-	return input_file_list
-
 if __name__ == "__main__":
-	input_list = get_input_list()
+	with open(INPUT_FILE_NAME) as input_file:
+		input_list = input_file.readlins()
