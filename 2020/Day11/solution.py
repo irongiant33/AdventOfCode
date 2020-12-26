f9cf5d869213e004278006a43c651acca9ff824e
irongiant33 <samuelmiller0714@gmail.com>
2020-12-25 22:38:05 -0500

    day 11 full solution

--- 2020/Day11/solution.py
+++ 2020/Day11/solution.py
@@ -5,26 +5,46 @@ INPUT_FILE_NAME = "input.txt"
 class Seat():
 	def set_neighbors(self,state):
 		self.neighbors = []
-		num_occupied = 0
+		num_occupied = [0,0]
 		for i in range(self.row-1,self.row+2):
 				row = []
 				for j in range(self.col-1,self.col+2):
 					row.append(state[i][j].property)
 					#don't count the cell itself
 					if(state[i][j].property=="#" and not (i == self.row and j == self.col)):
-						num_occupied = num_occupied + 1
+						num_occupied[0] = num_occupied[0] + 1
 				self.neighbors.append(row)
-		self.num_occupied = num_occupied
+		self.num_occupied[0] = num_occupied[0]
+
+		slopes = [[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1]]
+		for slope in slopes:
+			row = self.row
+			col = self.col
+			while(row > 0 and row < len(state)-1 and col > 0 and col < len(state[0])-1):
+				row = row + slope[0]
+				col = col + slope[1]
+				try:
+					if(state[row][col].property == "#"):
+						num_occupied[1] = num_occupied[1] + 1
+						break
+					elif(state[row][col].property == "L"):
+						break
+				except:
+					print(row)
+					print(col)
 		return num_occupied
 
 	def __init__(self,seat_chart,row,col,prop=''):
 		self.property=prop
-		self.num_occupied = 0
+		self.num_occupied = [0,0]
 		if(prop==''):
 			self.property = seat_chart[row][col]
 		self.neighbors = []
 		self.row = row
 		self.col = col
+
+	def __repr__(self):
+		return self.property
 
 def empty_row(length):
 	row = []
@@ -32,7 +52,13 @@ def empty_row(length):
 		row.append(Seat(['.'],0,0))
 	return row
 
-def update_states(state):
+def update_states(state,part2_flag):
+	index = 0
+	threshold = 4
+	#if part2
+	if(part2_flag):
+		index = 1
+		threshold = 5
 	update_flag = -1
 	new_state = []
 	new_state.append(empty_row(len(state[0])))
@@ -43,10 +69,10 @@ def update_states(state):
 			seat = state[i][j]
 			new_seat = Seat([],seat.row,seat.col,seat.property)
 			num_occupied = seat.set_neighbors(state)
-			if(seat.property == "L" and num_occupied == 0):
+			if(seat.property == "L" and num_occupied[index] == 0):
 				new_seat.property = "#"
 				update_flag = 0
-			elif(seat.property == "#" and num_occupied >= 4):
+			elif(seat.property == "#" and num_occupied[index] >= threshold):
 				new_seat.property = "L"
 				update_flag = 0
 			new_row.append(new_seat)
@@ -88,6 +114,7 @@ if __name__ == "__main__":
 
 		#create state0 for the seat states
 		seat_states = []
+		seat_states_pt2 = []
 		state0 = []
 		for row in range(0,len(seat_chart)):
 			seat_row = []
@@ -96,14 +123,26 @@ if __name__ == "__main__":
 				seat_row.append(seat)
 			state0.append(seat_row)
 		seat_states.append(state0)
+		seat_states_pt2 = copy.deepcopy(seat_states)
 
-		#update the seat states
+		#update the seat states (part 1)
 		update_flag = 0
 		while(update_flag == 0):
-			[update_flag,state] = update_states(seat_states[-1])
+			[update_flag,state] = update_states(seat_states[-1],False)
 			seat_states.append(state)
 			
 		#count occupied seats:
 		last_state = seat_states[-1]
 		count = count_states(last_state,False)
 		print("(part 1) The number of occupied seats in steady state is: "+str(count))
+
+		#update the seat states (part 2)
+		update_flag = 0
+		while(update_flag == 0):
+			[update_flag,state] = update_states(seat_states_pt2[-1],True)
+			seat_states_pt2.append(state)
+
+		#count occupied seats:
+		last_state_pt2 = seat_states_pt2[-1]
+		count2 = count_states(last_state_pt2,False)
+		print("(part 2) The number of occupied seats in steady state is: "+str(count2))
